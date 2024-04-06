6e655654bee09a3228b2e03d22e964c5321451d5
perenpempudo <166076109+perenpempudo@users.noreply.github.com>
2024-04-06 13:06:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Obtenemos el elemento del título utilizando su ID
+var titulo = document.getElementById("titulo");
+
+// Cambiamos el contenido del título
+titulo.innerHTML = "¡Hola, Mundo! desde JavaScript";
